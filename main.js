e3d37135bfbe44858d400ec17487c9894d416e2e
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2020-10-26 22:23:48 +0200

    pakeiciau ft

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function gamintiKate() {
     var image = document.createElement('img');
-    image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+    image.src = 'liucija.jpg';
     var div = document.getElementById('flexbox');
     div.appendChild(image);
 }
